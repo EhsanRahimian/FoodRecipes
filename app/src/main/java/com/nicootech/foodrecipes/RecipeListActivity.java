c57fe3660ee191e2e6a3442048f97f577e700f75
Ethan Rahimian <nicootech@gmail.com>
2021-06-07 16:01:32 -0700

    Testing Retrofit response Objects (RecipeResponse)

--- app/src/main/java/com/nicootech/foodrecipes/RecipeListActivity.java
+++ app/src/main/java/com/nicootech/foodrecipes/RecipeListActivity.java
@@ -7,6 +7,7 @@ import android.view.View;
 import com.nicootech.foodrecipes.models.Recipe;
 import com.nicootech.foodrecipes.request.RecipeApi;
 import com.nicootech.foodrecipes.request.ServiceGenerator;
+import com.nicootech.foodrecipes.request.responses.RecipeResponse;
 import com.nicootech.foodrecipes.request.responses.RecipeSearchResponse;
 
 import java.io.IOException;
@@ -25,7 +26,7 @@ public class RecipeListActivity extends BaseActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_recipe_list);
-        // testing our RecipeSearchResponse  by providing thr button test on activity_recipe_list
+        // testing our RecipeResponse  by providing thr button test on activity_recipe_list
         // and setting the on click listener here:
         findViewById(R.id.test).setOnClickListener(new View.OnClickListener() {
             @Override
@@ -37,21 +38,50 @@ public class RecipeListActivity extends BaseActivity {
     private void testRetrofitRequest(){
         RecipeApi recipeApi = ServiceGenerator.getRecipeApi();
 
-        Call<RecipeSearchResponse> responseCall = recipeApi
-                .searchRecipe(
-                        "chicken breast",
-                        "1"
+//        Call<RecipeSearchResponse> responseCall = recipeApi
+//                .searchRecipe(
+//                        "chicken breast",
+//                        "1"
+//                );
+//        responseCall.enqueue(new Callback<RecipeSearchResponse>() {
+//            @Override
+//            public void onResponse(Call<RecipeSearchResponse> call, Response<RecipeSearchResponse> response) {
+//                Log.d(TAG, "onResponse: server response: "+response.toString());
+//                if(response.code() == 200) {
+//                    Log.d(TAG, "onResponse: " + response.body().toString());
+//                    List<Recipe> recipes = new ArrayList<>(response.body().getRecipes());
+//                    for (Recipe recipe : recipes) {
+//                        Log.d(TAG, "onResponse: " + recipe.getTitle());
+//                    }
+//                }
+//                else{
+//                    try {
+//                        Log.d(TAG, "onResponse: "+response.errorBody().string());
+//
+//                    }catch (IOException e){
+//                        e.printStackTrace();
+//                    }
+//
+//                }
+//            }
+//
+//            @Override
+//            public void onFailure(Call<RecipeSearchResponse> call, Throwable t) {
+//
+//            }
+//        });
+        Call<RecipeResponse> responseCall = recipeApi
+                .getRecipe(
+                        "41470"
                 );
-        responseCall.enqueue(new Callback<RecipeSearchResponse>() {
+        responseCall.enqueue(new Callback<RecipeResponse>() {
             @Override
-            public void onResponse(Call<RecipeSearchResponse> call, Response<RecipeSearchResponse> response) {
+            public void onResponse(Call<RecipeResponse> call, Response<RecipeResponse> response) {
                 Log.d(TAG, "onResponse: server response: "+response.toString());
                 if(response.code() == 200) {
                     Log.d(TAG, "onResponse: " + response.body().toString());
-                    List<Recipe> recipes = new ArrayList<>(response.body().getRecipes());
-                    for (Recipe recipe : recipes) {
-                        Log.d(TAG, "onResponse: " + recipe.getTitle());
-                    }
+                    Recipe recipe = response.body().getRecipe();
+                    Log.d(TAG, "onResponse: RETRIEVED RECIPE: "+ recipe.toString());
                 }
                 else{
                     try {
@@ -65,7 +95,7 @@ public class RecipeListActivity extends BaseActivity {
             }
 
             @Override
-            public void onFailure(Call<RecipeSearchResponse> call, Throwable t) {
+            public void onFailure(Call<RecipeResponse> call, Throwable t) {
 
             }
         });
