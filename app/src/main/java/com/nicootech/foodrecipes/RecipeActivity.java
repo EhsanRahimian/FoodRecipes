c90e45911f01468541bb35547baf4f335b50c4a1
Ethan Rahimian <nicootech@gmail.com>
2021-06-15 15:21:24 -0700

    Building RecipeActivity

---
+++ app/src/main/java/com/nicootech/foodrecipes/RecipeActivity.java
@@ -0,0 +1,34 @@
+package com.nicootech.foodrecipes;
+
+import android.os.Bundle;
+import android.widget.LinearLayout;
+import android.widget.ScrollView;
+import android.widget.TextView;
+import com.nicootech.foodrecipes.models.Recipe;
+import androidx.annotation.Nullable;
+import androidx.appcompat.widget.AppCompatImageView;
+
+public class RecipeActivity extends BaseActivity {
+
+    private AppCompatImageView mRecipeImage;
+    private TextView mRecipeTitle, mRecipeRank;
+    private LinearLayout mRecipeIngredientsContainer;
+    private ScrollView mScrollView;
+    @Override
+    protected void onPostCreate(@Nullable Bundle savedInstanceState) {
+        super.onPostCreate(savedInstanceState);
+        setContentView(R.layout.activity_recipe);
+        mRecipeImage = findViewById(R.id.recipe_image);
+        mRecipeTitle =findViewById(R.id.recipe_title);
+        mRecipeRank = mRecipeTitle =findViewById(R.id.recipe_social_score);
+        mRecipeIngredientsContainer = findViewById(R.id.ingredients_container);
+        mScrollView = findViewById(R.id.parent);
+        getIncomingIntent();
+    }
+
+    private void getIncomingIntent(){
+      if(getIntent().hasExtra("recipe")){
+          Recipe recipe = getIntent().getParcelableExtra("recipe");
+      }
+    }
+}
